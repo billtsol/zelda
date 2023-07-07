0aeee587aeb4448cb4f7577aa17829ace79d8a38
Vasilis Tsolakidis <billtsol@protonmail.com>
2023-07-07 22:03:23 +0300

    Zelda game - Initial code

---
+++ code/level.py
@@ -0,0 +1,38 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+
+class Level:
+    def __init__(self):
+
+        # get the display serface
+        self.display_serface = pygame.display.get_surface()
+
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+
+                x = col_index * TILESIZE # calculate x position
+                y = row_index * TILESIZE # calculate y position
+
+                if col == 'x': # draw rock
+                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+
+                elif col == 'p': # draw player
+                    Player((x,y), [self.visible_sprites])
+
+
+
+
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.draw(self.display_serface) # draw the visible sprites
---
+++ code/main.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+    def __init__(self):
+
+        #general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) # set screen dimensions
+        pygame.display.set_caption('Zelda') # set title
+        self.clock = pygame.time.Clock()
+
+        self.level = Level() # initialize level
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+              if event.type == pygame.QUIT: # quit
+                  pygame.quit()
+                  sys.exit()
+
+            self.screen.fill('black') # make the screen black
+
+            self.level.run() # run level
+
+            pygame.display.update() # update the screen
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
